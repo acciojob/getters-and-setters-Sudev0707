fdf02bc676d9a94f0d3816e9aa1dcc4eaf651d08
SUDEV MAJHI <58890958+Sudev0707@users.noreply.github.com>
2026-05-19 10:34:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-//complete this code
-class Person {}
+// Complete this code
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+  
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+  
+  // Setter for age
+  set age(age) {
+    this._age = age;
+  }
+  
+  // Getter for age (to access age value)
+  get age() {
+    return this._age;
+  }
+}
 
-class Student extends Person {}
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
